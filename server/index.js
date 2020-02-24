4a603bcc9f4dcf0c3389c7223d73ad0e4c1553ff
Yuhang Chen <yuhangchen@Yuhangs-MacBook-Pro.local>
2020-02-23 23:34:55 -0800

    ready to depolu

---
+++ server/index.js
@@ -0,0 +1,11 @@
+require('newrelic');
+const express = require('express');
+let app = express();
+const port = 8080;
+const path = require('path');
+
+app.use('/:id', express.static(path.join(__dirname, '/../index.html')));
+
+app.listen(port, () => {
+  console.log(`proxy server listening on port ${port}`);
+});
